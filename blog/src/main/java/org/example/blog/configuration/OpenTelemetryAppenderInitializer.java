504f402a3e2784b6267e68ac6b724e930e936120
Anonymous <anon@gmail.com>
2026-09-10 20:54:56 +0200

    refractor

---
+++ blog/src/main/java/org/example/blog/configuration/OpenTelemetryAppenderInitializer.java
@@ -0,0 +1,22 @@
+package org.example.blog.configuration;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/** Connects Logback's OpenTelemetry appender to Spring Boot's OpenTelemetry SDK. */
+@Component
+class OpenTelemetryAppenderInitializer implements InitializingBean {
+
+    private final OpenTelemetry openTelemetry;
+
+    OpenTelemetryAppenderInitializer(OpenTelemetry openTelemetry) {
+        this.openTelemetry = openTelemetry;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        OpenTelemetryAppender.install(openTelemetry);
+    }
+}
